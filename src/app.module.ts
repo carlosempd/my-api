abcd2d0c5bb9e635b108518fd228b4ce85c9551c
Carlos <carlosempd@gmail.com>
2023-05-10 22:29:45 -0400

    update dev dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule, 
+    PostsModule, 
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
