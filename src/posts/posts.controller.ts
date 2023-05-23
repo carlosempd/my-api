4322e254ac0b21cf49d85bd488015a074693d3f6
Carlos <carlosempd@gmail.com>
2023-05-23 18:54:28 -0400

    add methods to list and update posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { 
+    Body, 
+    Controller, 
+    Delete, 
+    Get, 
+    Param, 
+    Post, 
+    Put, 
+    Req, 
+    UseGuards } from '@nestjs/common';
 import { CreatePostDto } from 'src/core/dto/createPost.dto';
 import { PostsService } from './posts.service';
 import { AuthGuard } from 'src/core/guards/auth/auth.guard';
@@ -11,5 +20,25 @@ export class PostsController {
     @Post()
     create(@Req() req: Request, @Body() createPostDto: CreatePostDto) {
         return this.postService.create(createPostDto, req['user']);
+    }
+
+    @Get()
+    findAll() {
+        return this.postService.findAll();
+    }
+
+    @Get(':id')
+    findById(@Param() params: { id: number }) {
+        return this.postService.findById(params.id);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        return this.postService.remove(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() updatePostDto) {
+        return this.postService.update(id, updatePostDto);
     }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreatePostDto } from 'src/core/dto/createPost.dto';
 import { Posts } from 'src/core/entity/post.entity';
@@ -16,12 +16,36 @@ export class PostsService {
     async create(
         body: CreatePostDto, 
         userFromToken: { email: string, sub: number }
-    ) {
+    ): Promise<Posts> {
         const post = new Posts();
         const user = await this.userService.findById(userFromToken.sub);
         post.name = body.name;
         post.text = body.text;
         post.author = user;
+
+        return this.postRepository.save(post);
+    }
+
+    findAll(): Promise<Posts[]> {
+        return this.postRepository.find();
+    }
+
+    findById(id: number): Promise<Posts> {
+        return this.postRepository.findOneBy({ id });
+    }
+
+    async remove (id: number) {
+        return await this.postRepository.softDelete({ id });
+    }
+
+    async update(id: number, updatePostDto): Promise<Posts> {
+        const post = await this.findById(id);
+
+        if (!post) {
+            throw new BadRequestException({ message: 'Post doesn\'t exist' })
+        }
+
+        Object.assign(post, updatePostDto);
 
         return this.postRepository.save(post);
     }
